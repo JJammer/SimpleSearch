3836d3e237abc44f34ae300d268df85c39051f24
Vladyslava Shevchuk <sh.vladyslava@gmail.com>
2019-12-11 18:51:59 +0200

    add parsing and its tests

--- src/main/scala/search/Index.scala
+++ src/main/scala/search/Index.scala
@@ -2,6 +2,9 @@ package search
 
 import java.io.File
 
+import scala.io._
+import scala.util.Using
+
 case class Index private[search](map: Map[String, Set[String]]) {
 
   // TODO add logic
@@ -10,6 +13,24 @@ case class Index private[search](map: Map[String, Set[String]]) {
 
 object Index {
 
-  //TODO add parsing logic
-  def apply(file: File): Index = new Index(Map.empty)
+  def apply(file: File): Index = {
+
+    def parseFile(file: File): Set[String] =
+      Using(Source.fromFile(file)) {
+        source => source.getLines().flatMap(x => x.split("\\s")).filter(!_.isBlank).toSet
+      }.getOrElse(Set.empty)
+
+    def parse(file: File, acc: Map[String, Set[String]] = Map.empty, unread: List[File] = List.empty) : Map[String, Set[String]] = {
+
+      val files = if (file.isDirectory) unread ++ file.listFiles().toList else unread
+      val localAcc = if (file.isDirectory) acc else acc + (file.getName -> parseFile(file))
+
+      files match {
+        case Nil => localAcc
+        case head :: tail => parse(head, localAcc, tail)
+      }
+    }
+
+    new Index(parse(file))
+  }
 }
--- src/test/scala/search/IndexTest.scala
+++ src/test/scala/search/IndexTest.scala
@@ -2,6 +2,7 @@ package search
 
 import org.scalatest._
 import org.scalatest.matchers.should.Matchers
+import java.io.File
 
 class IndexTest extends FlatSpec with Matchers {
 
@@ -10,5 +11,31 @@ class IndexTest extends FlatSpec with Matchers {
     val index = new Index(map)
 
     index.search(Set("orange", "apple", "banana")) should be (Map[String, Double]("file1" -> 100))
+  }
+
+  "Parse folder with files" should "return index with content of all readable files" in {
+    val expected = new Index(Map(
+      "file1.txt" -> Set("word2", "565464335", "word3", "word1", "word4"),
+      "file2.txt" -> Set("apple", "banana", "word45")
+    ))
+    val file = new File("src//test//resources//test1")
+
+    Index(file) should be (expected)
+  }
+
+  "Parse folder with subfolders" should "return index with content of all readable files in folder and subfolders" in {
+    val expected = new Index(Map(
+      "file1.txt" -> Set("word2", "565464335", "word3", "word1", "word4"),
+      "file2.txt" -> Set("apple", "banana", "word45")
+    ))
+    val file = new File("src//test//resources//test2")
+
+    Index(file) should be (expected)
+  }
+
+  "Parse folder with no files" should "return index with no content" in {
+    val file = new File("src//test//resources//test3")
+
+    Index(file) should be (new Index(Map.empty))
   }
 }
